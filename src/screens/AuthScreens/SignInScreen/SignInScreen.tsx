c85e0542e98896add6e512f1a60a1bbfc0eb293f
Artem Magei <artemmagej@gmail.com>
2023-03-26 17:56:59 +0300

    feat: add basic screens mock

---
+++ src/screens/AuthScreens/SignInScreen/SignInScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const SignInScreen = () => {
+  return (
+    <View>
+      <Text>SignInScreen</Text>
+    </View>
+  );
+};
