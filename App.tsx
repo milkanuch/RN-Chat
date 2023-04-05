cd06b7cafba01b12da9e69943986285a1325fee1
Artem Magei <artemmagej@gmail.com>
2023-04-05 18:51:25 +0300

    feat: add font load check

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
+import { useLoadFonts } from './src/hooks/useLoadFonts';
+
 import { AppNavigation } from 'navigation/AppNavigation/AppNavigation';
 
 const App = () => {
+  const { isFontsLoaded } = useLoadFonts();
+  if (!isFontsLoaded) {
+    return null;
+  }
   return <AppNavigation />;
 };
-
 //NOTE: Due to expo specific, we need to disable this rule here
 // eslint-disable-next-line import/no-default-export
 export default App;
