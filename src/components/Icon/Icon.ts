4f3531440dc132b3bc617d524180ea422af2ba6b
Artem Magei <artemmagej@gmail.com>
2023-04-06 19:11:57 +0300

    refactor: add icon settings file

--- src/components/Icon/Icon.ts
+++ src/components/Icon/Icon.ts
@@ -1,7 +1,14 @@
 import { createIconSetFromIcoMoon } from '@expo/vector-icons';
 
+import {
+  ICO_MOON_FONT_NAME,
+  ICO_MOON_TTF_NAME,
+} from 'components/Icon/Icon.settings';
+
+import { ICO_MOON_SELECTION_JSON } from 'constants/fonts/fonts';
+
 export const Icon = createIconSetFromIcoMoon(
-  require('assets/fonts/icomoon/selection.json'),
-  'IcoMoon',
-  'icomoon.ttf',
+  ICO_MOON_SELECTION_JSON,
+  ICO_MOON_FONT_NAME,
+  ICO_MOON_TTF_NAME,
 );
--- src/constants/fonts/fonts.ts
+++ src/constants/fonts/fonts.ts
@@ -1,1 +1,2 @@
 export const ICO_MOON = require('assets/fonts/icomoon/icomoon.ttf');
+export const ICO_MOON_SELECTION_JSON = require('assets/fonts/icomoon/selection.json');
