fb29d59bfcca014cc6f06689e1fd12b2d8d0b42c
Artem Magei <artemmagej@gmail.com>
2023-04-08 19:38:13 +0300

    feat: create custom text input

---
+++ src/components/CustomTextInput/customTextInput.styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+import { COLORS } from 'constants/color';
+
+export const styles = StyleSheet.create({
+  input: {
+    padding: 8,
+    paddingLeft: 14,
+  },
+  inputContainer: {
+    backgroundColor: COLORS.light.primary[200],
+  },
+});
---
+++ src/components/CustomTextInput/customTextInput.types.ts
@@ -0,0 +1,3 @@
+import { TextInput } from 'react-native';
+
+export type CustomTextInputProps = TextInput['props'];
