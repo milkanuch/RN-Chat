ae1977bfe26487049f9feb066f869964aaebe603
milkanuch <milkanic2@gmail.com>
2023-04-17 16:52:43 +0300

    feat: add sign in screen content

--- src/screens/AuthScreens/SignInScreen/SignInScreen.tsx
+++ src/screens/AuthScreens/SignInScreen/SignInScreen.tsx
@@ -1,10 +1,199 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { FC, useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
 
-export const SignInScreen = () => {
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, Controller } from 'react-hook-form';
+import MaskInput from 'react-native-mask-input';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { AnimatedButton } from 'components/AnimatedButton/AnimatedButton';
+import { CustomTextInput } from 'components/CustomTextInput/CustomTextInput';
+
+import { COLORS } from 'constants/color';
+
+import { SignInScreenTitle } from './SignInScreenTitle/SignInScreenTitle';
+import { signInScheme } from './signInScreen.schema';
+import { getAuthorizationProgress } from './signInScreen.utils';
+
+import {
+  FORM_MODE,
+  DEFAULT_VALUE,
+  PASSWORD_SETTINGS,
+  PHONE_NUMBER_SETTINGS,
+  CONFIRM_PASSWORD_SETTINGS,
+  SHAKE_ANIMATION,
+  SHAKE_ANIMATION_CONFIG,
+  PROGRESS_ANIMATION_DURATION,
+} from './signInScreen.settings';
+import { styles } from './signInScreen.styles';
+import { SignInForm } from './signInScreen.types';
+import {
+  AuthStackScreenTypes,
+  SignInScreenProp,
+} from 'navigation/AuthStackNavigation/authStackNavigation.types';
+
+export const SignInScreen: FC<SignInScreenProp> = ({ navigation }) => {
+  const [isUserRegistered, setIsUserRegistered] = useState<boolean>(true);
+  const authorizationProgress = useSharedValue<number>(0);
+
+  const {
+    control,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<SignInForm>({
+    resolver: yupResolver(signInScheme),
+    mode: FORM_MODE,
+  });
+
+  const buttonTitle = isUserRegistered ? 'SIGN UP' : 'CONTINUE';
+  const isButtonDisabled = !!Object.keys(errors).length;
+
+  useEffect(() => {
+    const values = getValues();
+
+    authorizationProgress.value = withTiming(
+      getAuthorizationProgress(errors, values, isUserRegistered),
+      PROGRESS_ANIMATION_DURATION,
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    errors.phoneNumber,
+    errors.password,
+    errors.confirmPassword,
+    isUserRegistered,
+  ]);
+
+  const buttonProgressStyle = useAnimatedStyle(() => {
+    return {
+      width: `${authorizationProgress.value}%`,
+      height: 50,
+      backgroundColor: COLORS.darkBlue,
+    };
+  }, [authorizationProgress]);
+
+  const buttonShakeStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: withSequence(
+            withTiming(SHAKE_ANIMATION.left, SHAKE_ANIMATION_CONFIG),
+            withTiming(SHAKE_ANIMATION.right, SHAKE_ANIMATION_CONFIG),
+            withTiming(SHAKE_ANIMATION.left, SHAKE_ANIMATION_CONFIG),
+            withTiming(SHAKE_ANIMATION.right, SHAKE_ANIMATION_CONFIG),
+          ),
+        },
+      ],
+    };
+  }, [authorizationProgress.value]);
+
+  const handleContinue = () => {
+    if (!isUserRegistered) {
+      navigation.navigate(AuthStackScreenTypes.SignUp);
+      return;
+    }
+    setIsUserRegistered(true);
+  };
+
   return (
-    <View>
-      <Text>SignInScreen</Text>
-    </View>
+    <SafeAreaView style={styles.screen}>
+      <SignInScreenTitle />
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.label}>{PHONE_NUMBER_SETTINGS.label}</Text>
+          <View style={[styles.inputContainer, styles.phoneNumberContainer]}>
+            <Text style={styles.countryCode}>
+              {PHONE_NUMBER_SETTINGS.countryCode}
+            </Text>
+            <Controller
+              control={control}
+              defaultValue={DEFAULT_VALUE}
+              name={PHONE_NUMBER_SETTINGS.name}
+              render={({ field: { onChange: handlePhoneNumber, value } }) => (
+                <MaskInput
+                  keyboardType={PHONE_NUMBER_SETTINGS.keyboardType}
+                  mask={PHONE_NUMBER_SETTINGS.keyboardMask}
+                  maxLength={PHONE_NUMBER_SETTINGS.maxLength}
+                  onChangeText={handlePhoneNumber}
+                  placeholder={PHONE_NUMBER_SETTINGS.placeholder}
+                  style={[styles.input, styles.phoneNumberInput]}
+                  value={value}
+                />
+              )}
+            />
+          </View>
+          {!!errors.phoneNumber && (
+            <Text style={styles.error}>{errors.phoneNumber.message}</Text>
+          )}
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Controller
+            control={control}
+            defaultValue={DEFAULT_VALUE}
+            name={PASSWORD_SETTINGS.name}
+            render={({ field: { onChange: handlePhoneNumber, value } }) => (
+              <CustomTextInput
+                label={PASSWORD_SETTINGS.label}
+                onChangeText={handlePhoneNumber}
+                placeholder={PASSWORD_SETTINGS.placeholder}
+                secureTextEntry={true}
+                style={styles.input}
+                value={value}
+              />
+            )}
+          />
+          {!!errors.password && (
+            <Text style={styles.error}>{errors.password.message}</Text>
+          )}
+        </View>
+
+        {!isUserRegistered && (
+          <View style={styles.inputContainer}>
+            <Controller
+              control={control}
+              defaultValue={DEFAULT_VALUE}
+              name={CONFIRM_PASSWORD_SETTINGS.name}
+              render={({ field: { onChange: handlePhoneNumber, value } }) => (
+                <CustomTextInput
+                  label={CONFIRM_PASSWORD_SETTINGS.label}
+                  onChangeText={handlePhoneNumber}
+                  placeholder={CONFIRM_PASSWORD_SETTINGS.placeholder}
+                  secureTextEntry={true}
+                  style={styles.input}
+                  value={value}
+                />
+              )}
+            />
+            {!!errors.confirmPassword && (
+              <Text style={styles.error}>{errors.confirmPassword.message}</Text>
+            )}
+          </View>
+        )}
+
+        <AnimatedButton
+          animatedStyle={buttonShakeStyle}
+          disabled={isButtonDisabled}
+          onPress={handleSubmit(handleContinue)}
+          style={styles.button}>
+          <Animated.View style={buttonShakeStyle}>
+            <Animated.View style={buttonProgressStyle} />
+            <Text
+              style={[
+                styles.buttonLabel,
+                isButtonDisabled && styles.disabledButtonLabel,
+              ]}>
+              {buttonTitle}
+            </Text>
+          </Animated.View>
+        </AnimatedButton>
+      </View>
+    </SafeAreaView>
   );
 };
---
+++ src/screens/AuthScreens/SignInScreen/signInScreen.schema.ts
@@ -0,0 +1,34 @@
+import * as yup from 'yup';
+
+import {
+  PASSWORD_SETTINGS,
+  PHONE_NUMBER_SETTINGS,
+  CONFIRM_PASSWORD_SETTINGS,
+} from './signInScreen.settings';
+
+const MIN_PASSWORD_LENGTH = 8;
+
+const PHONE_NUMBER_REGEX = /^\((67|68|96|97|98)\) \d{3}-\d{4}$/;
+
+const PASSWORD_REGEX = /(?=.*[0-9])/;
+
+export const signInScheme = yup
+  .object({
+    [PHONE_NUMBER_SETTINGS.name]: yup
+      .string()
+      .matches(PHONE_NUMBER_REGEX, 'Please, provide valid  phone number.')
+      .required('Phone number is required'),
+    [PASSWORD_SETTINGS.name]: yup
+      .string()
+      .uppercase('Password must contain a capital letter.')
+      .matches(PASSWORD_REGEX, 'Password must contain a number.')
+      .min(
+        MIN_PASSWORD_LENGTH,
+        `Password must be at least ${MIN_PASSWORD_LENGTH} characters.`,
+      )
+      .required('Password is required'),
+    [CONFIRM_PASSWORD_SETTINGS.name]: yup
+      .string()
+      .oneOf([yup.ref(`${PASSWORD_SETTINGS.name}`)], "Passwords doesn't match"),
+  })
+  .required();
