8b1216f349dbbfba58389835ecee312a99ba7ebe
Artem Magei <artemmagej@gmail.com>
2023-04-20 14:43:08 +0300

    feat: create empty content component

---
+++ src/components/EmptyContent/emptyContent.types.ts
@@ -0,0 +1,10 @@
+import { TouchableOpacityProps } from 'react-native';
+
+export interface EmptyContentProps {
+  title: string;
+  description: string;
+  icon: string;
+  buttonTitle: string;
+  iconSize?: number;
+  onPress: TouchableOpacityProps['onPress'];
+}
