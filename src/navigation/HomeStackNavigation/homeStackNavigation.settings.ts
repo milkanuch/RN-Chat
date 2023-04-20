dec0f5bf2d32e36748785dfe5ecfa5beb6869051
Artem Magei <artemmagej@gmail.com>
2023-04-20 17:28:31 +0300

    feat: create home screen header

--- src/navigation/HomeStackNavigation/homeStackNavigation.settings.ts
+++ src/navigation/HomeStackNavigation/homeStackNavigation.settings.ts
@@ -1,5 +1,5 @@
 import { StackNavigationOptions } from '@react-navigation/stack';
 
 export const HOME_SCREEN_OPTIONS: StackNavigationOptions = {
-  headerShadowVisible: false,
+  headerShown: false,
 };
