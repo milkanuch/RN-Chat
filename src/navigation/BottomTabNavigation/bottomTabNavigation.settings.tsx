b28f9b8aab744977e064f7b04561cc36466538b1
Artem Magei <artemmagej@gmail.com>
2023-04-20 21:14:36 +0300

    feat: add style sheet for bottom tab navigation

--- src/navigation/BottomTabNavigation/bottomTabNavigation.settings.tsx
+++ src/navigation/BottomTabNavigation/bottomTabNavigation.settings.tsx
@@ -1,28 +1,10 @@
 import { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
 
-import { Icon } from 'components/Icon/Icon';
-
-import { COLORS } from 'constants/color';
+import { styles } from 'navigation/BottomTabNavigation/bottomTabNavigation.styles';
 
 export const HOME_STACK_OPTIONS: BottomTabNavigationOptions = {
   headerShown: false,
   tabBarLabel: 'Chats',
-  tabBarStyle: {
-    backgroundColor: COLORS.light.primary[400],
-    borderTopWidth: 0,
-    elevation: 0,
-  },
-  tabBarLabelStyle: {
-    fontSize: 12,
-    fontWeight: 'bold',
-  },
-  tabBarIcon: ({ focused }) => {
-    return (
-      <Icon
-        color={focused ? COLORS.blue : COLORS.grey}
-        name="message"
-        size={22}
-      />
-    );
-  },
+  tabBarStyle: styles.tabBarStyle,
+  tabBarLabelStyle: styles.tabBarLabelStyle,
 };
