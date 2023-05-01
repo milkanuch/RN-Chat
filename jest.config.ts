e250b6efa8f2e5e3310391d2240e0a788badd962
milkanuch <milkanic2@gmail.com>
2023-05-01 13:31:56 +0300

    feat: add coverage action

--- jest.config.ts
+++ jest.config.ts
@@ -4,14 +4,16 @@ const config: Config = {
   verbose: true,
   clearMocks: true,
   preset: 'jest-expo',
-  collectCoverage: true,
   collectCoverageFrom: [
-    '**/*.{ts,tsx}',
+    'src/**/*.{js,jsx,ts,tsx}',
     '!**/coverage/**',
     '!**/node_modules/**',
     '!**/babel.config.js',
-    '!**/jest.config.ts',
+    '!**/jest.setup.js',
+    '!.eslintrc.js',
+    '!.prettierrc.js',
   ],
+  coverageReporters: ['json-summary'],
   //SOURCE: https://docs.expo.dev/guides/testing-with-jest/#configuration
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
