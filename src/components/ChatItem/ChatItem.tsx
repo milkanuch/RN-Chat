16010c3d738b98c8c31a4493b5991bd4e339871d
Artem Magei <artemmagej@gmail.com>
2023-05-16 23:20:12 +0300

    feat: update chat item

--- src/components/ChatItem/ChatItem.tsx
+++ src/components/ChatItem/ChatItem.tsx
@@ -6,7 +6,9 @@ import { Image } from 'expo-image';
 
 import {
   ELLIPSIZE_MODE,
-  LAST_MESSAGE_LABEL,
+  IS_OFFLINE_LABEL,
+  IS_ONLINE_LABEL,
+  IS_TYPING_LABEL,
   MAX_LINES,
   MAX_UNREAD_MESSAGES,
   MAX_UNREAD_MESSAGES_LABEL,
@@ -25,32 +27,54 @@ export const ChatItem: FC<ChatItemProps> = ({
   isOnline,
   unreadMessages,
   avatar,
+  disabled,
+  groupChat,
 }) => {
   const { navigate } = useNavigation();
   const handleOpenChat = () => navigate(AppStackTypes.ChatScreen, { id });
 
   return (
-    <TouchableOpacity onPress={handleOpenChat} style={styles.container}>
+    <TouchableOpacity
+      disabled={disabled}
+      onPress={handleOpenChat}
+      style={styles.container}>
       <View style={styles.avatarContainer}>
-        <Image source={avatar} style={styles.avatar} />
+        <Image
+          source={{
+            uri: avatar,
+          }}
+          style={styles.avatar}
+        />
         {!!isOnline && <View style={styles.onlineIndicator} />}
       </View>
-      <View style={styles.infoContainer}>
+      <View style={[styles.infoContainer, !!lastMessageTime && styles.flex]}>
         <View style={styles.usernameContainer}>
           <Text style={styles.username}>{username}</Text>
-          <Text style={[styles.dateTime, styles.text]}>{lastMessageTime}</Text>
+          {!!lastMessageTime && (
+            <Text style={[styles.dateTime, styles.text]}>
+              {lastMessageTime}
+            </Text>
+          )}
         </View>
         <View style={styles.messageContainer}>
           {isTyping ? (
-            <Text style={[styles.message, styles.text]}>
-              {username} {LAST_MESSAGE_LABEL}
+            <Text style={[styles.message, styles.flex, styles.text]}>
+              {groupChat ? `${username} ${IS_TYPING_LABEL}` : IS_TYPING_LABEL}
             </Text>
           ) : (
             <Text
               ellipsizeMode={ELLIPSIZE_MODE}
               numberOfLines={MAX_LINES}
-              style={[styles.message, styles.text]}>
-              {lastMessage}
+              style={[
+                styles.message,
+                styles.text,
+                !!lastMessage && styles.flex,
+              ]}>
+              {lastMessage
+                ? lastMessage
+                : isOnline
+                ? IS_ONLINE_LABEL
+                : IS_OFFLINE_LABEL}
             </Text>
           )}
           {!!unreadMessages && (
--- src/components/ChatItem/chatItem.settings.ts
+++ src/components/ChatItem/chatItem.settings.ts
@@ -1,5 +1,7 @@
 export const ELLIPSIZE_MODE = 'tail';
 export const MAX_LINES = 1;
 export const MAX_UNREAD_MESSAGES = 9;
-export const LAST_MESSAGE_LABEL = 'is typing...';
+export const IS_TYPING_LABEL = 'is typing...';
+export const IS_ONLINE_LABEL = 'online';
+export const IS_OFFLINE_LABEL = 'offline';
 export const MAX_UNREAD_MESSAGES_LABEL = '9+';
