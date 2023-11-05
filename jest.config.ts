62bd3685f8c1820e2ca770a34c530c59984fa525
Misha Ilkanych <misha.ilkanych@ekreative.com>
2023-11-05 19:54:04 +0200

    feat: update jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
     '!.prettierrc.js',
   ],
   coverageReporters: ['json-summary'],
+  setupFilesAfterEnv: ['./src/constants/mocks/server.ts'],
   //SOURCE: https://docs.expo.dev/guides/testing-with-jest/#configuration
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
